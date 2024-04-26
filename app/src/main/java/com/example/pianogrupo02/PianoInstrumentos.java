8eda0b08925c350bbd87e753f46ad08aef9a5da8
AndyRoldan <andy.guevara@catolica.edu.sv>
2024-04-26 04:09:01 -0600

    Piano intrumentos terminada

--- app/src/main/java/com/example/pianogrupo02/PianoInstrumentos.java
+++ app/src/main/java/com/example/pianogrupo02/PianoInstrumentos.java
@@ -5,13 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.google.android.material.snackbar.Snackbar;
 
 public class PianoInstrumentos extends AppCompatActivity {
+
+    private MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,4 +52,40 @@ public class PianoInstrumentos extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+    public void playAnimal(int soundResourceId) {
+
+        if (mediaPlayer != null && mediaPlayer.isPlaying()) {
+            mediaPlayer.stop();
+            mediaPlayer.release();
+            mediaPlayer = null;
+        }
+        mediaPlayer = MediaPlayer.create(PianoInstrumentos.this, soundResourceId);
+        try {
+            mediaPlayer.start();
+        } catch (IllegalStateException e) {
+            e.printStackTrace();
+        }
+    }
+    public void serpiente(View v) {
+        playAnimal(R.raw.tambores);
+    }
+    public void tigre(View v) {
+        playAnimal(R.raw.trompeta);
+    }
+    public void mono(View v) {
+        playAnimal(R.raw.maracas);
+    }
+    public void mapache(View v) {
+        playAnimal(R.raw.violin);
+    }
+    public void cocodrilo(View v) {
+        playAnimal(R.raw.guitarra);
+    }
+    public void oso(View v) {
+        playAnimal(R.raw.pandereta);
+    }
+    public void elefante(View v) {
+        playAnimal(R.raw.acordeon);
+    }
+
 }
